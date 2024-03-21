23781af605cee19b74c0f55768b919dd899bc084
user-rutwik-at-354754763556 <rutwikbaheti123@gmail.com>
2024-03-21 08:38:12 +0530

    Initial commit

---
+++ src/main/java/com/ecart/user/EcartUserMsApplication.java
@@ -0,0 +1,13 @@
+package com.ecart.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcartUserMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcartUserMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecart/user/EcartUserMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecart.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcartUserMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
